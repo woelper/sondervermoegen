e6f02b047f69c1eb08e9701189f74c9494e76f4a
Johann Woelper <woelper@gmail.com>
2022-06-04 22:37:54 +0200

    basic functionality

--- src/app.rs
+++ src/app.rs
@@ -1,24 +1,23 @@
+mod resources;
+use std::collections::HashMap;
 
-use std::fs::File;
-
-use serde::{Serialize, Deserialize};
-use serde_json;
+use resources::*;
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    // Example stuff:
-    label: String,
     credits: i64,
+    units: Vec<Unit>,
+    units_bought: HashMap<Unit, usize>
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
             credits: 100_000_000_000,
+            units: serde_json::from_str(include_str!("../units.json")).unwrap(),
+            units_bought: HashMap::default()
         }
     }
 }
@@ -35,6 +34,8 @@ impl TemplateApp {
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         }
 
+
+
         Default::default()
     }
 }
@@ -48,7 +49,11 @@ impl eframe::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        let Self { label, credits } = self;
+        let Self {
+            credits,
+            units,
+            units_bought
+        } = self;
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
@@ -67,14 +72,19 @@ impl eframe::App for TemplateApp {
         });
 
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
+            ui.heading("Einheiten zum Kauf");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
 
-            ui.label(format!("Restvermoegen {}", credits));
+            for unit in units {
+                ui.horizontal(|ui| {
+                    ui.label(&unit.name);
+                    ui.label(format!("{}",unit.kaufpreis));
+                    if ui.button("Kaufen").clicked() {
+                        *units_bought.entry(unit.clone()).or_default() += 1;
+                    }
+
+                });
+            }
 
             // ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
             // if ui.button("Increment").clicked() {
@@ -94,14 +104,15 @@ impl eframe::App for TemplateApp {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
+            ui.heading(format!("Restvermoegen {}", credits));
 
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
+
+            for (unit, num) in units_bought {
+                ui.label(format!("{} x {} {}", num, &unit.name, *num as i64 * unit.total_cost()));
+
+            }
+
+          
         });
 
         if false {
@@ -113,52 +124,4 @@ impl eframe::App for TemplateApp {
             });
         }
     }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-
-enum Besoldung {
-    A3 = 2476,
-    A4 = 2553,
-    A5 = 2598,
-    A6 = 2700,
-    A7 = 2864,
-    A8 = 3090,
-    A9 = 3333,
-    A10 = 3654,
-    A11 = 4192,
-    A12 = 4565,
-    A13 = 5193,
-    A14 = 5508,
-    A15 = 6330,
-    A16 = 7018
-}
-
-impl Default for Besoldung {
-    fn default() -> Self {
-        Self::A5
-    }
-}
-
-#[derive(Debug, Default, Serialize, Deserialize)]
-
-struct Soldat {
-    besoldung: Besoldung
-}
-
-#[derive(Debug, Default, Serialize, Deserialize)]
-struct Unit {
-    name: String,
-    kaufpreis: i64,
-    udl: String,
-    soldaten: Vec<Soldat>,
-    wartung_pro_jahr: i64,
-}
-
-#[test]
-fn write_unit() {
-
-    let mut leopard2 = Unit::default();
-    leopard2.soldaten.push(Soldat::default());
-    serde_json::to_writer_pretty(File::create("leo.json").unwrap(), &leopard2).unwrap();
 }
---
+++ src/app/resources.rs
@@ -0,0 +1,60 @@
+use std::fs::File;
+
+use serde::{Serialize, Deserialize};
+use serde_json;
+
+
+
+#[derive(Debug, Serialize, Deserialize, Copy, Clone, Eq, PartialEq, Hash)]
+
+pub enum Besoldung {
+    A3 = 2476,
+    A4 = 2553,
+    A5 = 2598,
+    A6 = 2700,
+    A7 = 2864,
+    A8 = 3090,
+    A9 = 3333,
+    A10 = 3654,
+    A11 = 4192,
+    A12 = 4565,
+    A13 = 5193,
+    A14 = 5508,
+    A15 = 6330,
+    A16 = 7018
+}
+
+impl Default for Besoldung {
+    fn default() -> Self {
+        Self::A5
+    }
+}
+
+#[derive(Debug, Default, Serialize, Deserialize, Hash, Eq, Clone, PartialEq)]
+
+pub struct Soldat {
+    pub besoldung: Besoldung
+}
+
+#[derive(Debug, Default, Serialize, Deserialize, Clone, Hash, Eq, PartialEq)]
+pub struct Unit {
+    pub name: String,
+    pub kaufpreis: i64,
+    pub url: String,
+    pub soldaten: Vec<Soldat>,
+    pub wartung_pro_jahr: i64,
+}
+
+impl Unit {
+    pub fn total_cost(&self) -> i64 {
+        self.kaufpreis + self.soldaten.iter().fold(0, |sum, soldat| sum + soldat.besoldung as i64 * 12) + self.wartung_pro_jahr
+    }
+}
+
+#[test]
+fn write_unit() {
+
+    let mut leopard2 = Unit::default();
+    leopard2.soldaten.push(Soldat::default());
+    serde_json::to_writer_pretty(File::create("leo.json").unwrap(), &leopard2).unwrap();
+}
